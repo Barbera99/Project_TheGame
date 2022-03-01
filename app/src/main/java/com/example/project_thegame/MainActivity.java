35ae322f538cc3d79d50011c704ca1708e7dd9d1
Joan993 <46020017+Joan993@users.noreply.github.com>
2022-03-01 18:13:42 +0100

    FirstCommitJG

--- app/src/main/java/com/example/project_thegame/MainActivity.java
+++ app/src/main/java/com/example/project_thegame/MainActivity.java
@@ -1,16 +1,39 @@
 package com.example.project_thegame;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+    Button btnAboutPage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAboutPage = findViewById(R.id.btnAbPg);
+
+        btnAboutPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent_about_page = new Intent(MainActivity.this, AboutPageActivity.class);
+                startActivity(intent_about_page);
+            }
+        });
+
     }
+
+
+
+
 }
--- app/src/main/java/com/example/project_thegame/SplashScreenActivity.java
+++ app/src/main/java/com/example/project_thegame/SplashScreenActivity.java
@@ -3,18 +3,46 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class SplashScreenActivity extends Activity {
-
+    public static int SPLASH_TIME_OUT = 2000;
+    public final int REQUEST_CODE_B = 1;
+    public final int REQUEST_CODE_C = 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreenactivity);
 
+
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+                boolean agreed = prefs.getBoolean("agreed", false);
+                if (!agreed) {
+                    Intent termsCond = new Intent(SplashScreenActivity.this, TermConditionActivity.class);
+                    termsCond.addCategory(Intent.CATEGORY_HOME);
+                    termsCond.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivityForResult(termsCond,REQUEST_CODE_B);
+
+                } else {
+                    Intent mainActivity = new Intent(SplashScreenActivity.this, MainActivity.class);
+                    SplashScreenActivity.this.startActivity(mainActivity);
+                }
+            }
+        },SPLASH_TIME_OUT);
+
+
+        /*System.out.println("asdfasdfasdfsadf");
+        Timer temps = new Timer();
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
@@ -23,9 +51,22 @@ public class SplashScreenActivity extends Activity {
                 finish();
             }
         };
+        temps.schedule(task, 5000);*/
+    }
 
-        Timer temps = new Timer();
-        temps.schedule(task, 5000);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1){
+            boolean flag = data.getBooleanExtra("datBol",false);
+            if(flag){
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            } else {
+                System.exit(0);
+            }
+        }
 
     }
 }
