8d2e7a1befaad49a0d52266e592c4c82bcb16708
aleixBY23 <43808251+aleixBY23@users.noreply.github.com>
2022-05-04 21:21:58 +0200

    Ara si que register acabat pero no li apetix funcionar

--- app/src/main/java/com/example/project_thegame/repositories/UserRepo.java
+++ app/src/main/java/com/example/project_thegame/repositories/UserRepo.java
@@ -4,6 +4,8 @@ import android.util.Log;
 
 import androidx.lifecycle.MutableLiveData;
 
+import com.example.project_thegame.helpers.ApiCallBack;
+import com.example.project_thegame.models.Result;
 import com.example.project_thegame.service.UserServiceImpl;
 import com.example.project_thegame.service.UserService;
 import com.example.project_thegame.models.User;
@@ -15,10 +17,14 @@ import retrofit2.Response;
 public class UserRepo {
     private static String TAG = "UserRepo";
     private UserService userService;
+    private Result<Integer> registerResult;
+    private MutableLiveData<Result<Integer>> registerResultLiveData;
     public MutableLiveData<User> mplayer;
+
     public UserRepo() {
         this.userService = new UserServiceImpl();
         this.mplayer = new MutableLiveData<>();
+        registerResultLiveData = new MutableLiveData<>();
     }
 
     public void getUserByUsername(String username){
@@ -41,6 +47,33 @@ public class UserRepo {
     }
 
     public void registerUser(User user) {
+        this.userService.createUser(user).enqueue(new ApiCallBack<User>() {
 
+            @Override
+            public void onFailure(Call call, Throwable t) {
+                registerResult = Result.error(t);
+                registerResultLiveData.postValue(registerResult);
+                Log.d(TAG, "register() -> onResponseError -> " + t.getMessage());
+            }
+
+            @Override
+            public void onResponseSuccess(Call<User> call, Response<User> response) {
+                Log.d(TAG, "Int response");
+                Log.d(TAG, "register() -> onResponseSuccess -> " + response.body().toString());
+                int id_user = response.body().getId();
+                Log.d(TAG, "register() -> onResponseSuccess -> " + id_user);
+                registerResult = Result.success(id_user);
+                Log.d(TAG, "register() -> onResponseSuccess / getResult-> " + registerResult.getResult());
+                registerResultLiveData.postValue(registerResult);
+                Log.d(TAG, "register() -> onResponseSuccess END");
+            }
+
+            @Override
+            public void onResponseError(Call<User> call, Throwable t) {
+                registerResult = Result.error(t);
+                registerResultLiveData.postValue(registerResult);
+                Log.d(TAG, "register() -> onResponseError -> " + t.getMessage());
+            }
+        });
     }
 }
