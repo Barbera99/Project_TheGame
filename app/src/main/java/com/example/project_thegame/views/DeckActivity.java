97aef4f8ceca8c16a1f0900864579c03e31ba34e
Barbera99 <josep.barbera.reverte.1999@gmail.com>
2022-05-23 17:11:20 +0100

    DeckAcvitity

--- app/src/main/java/com/example/project_thegame/views/DeckActivity.java
+++ app/src/main/java/com/example/project_thegame/views/DeckActivity.java
@@ -1,8 +1,10 @@
-package com.example.project_thegame;
+package com.example.project_thegame.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.project_thegame.R;
 
 public class DeckActivity extends AppCompatActivity {
 
