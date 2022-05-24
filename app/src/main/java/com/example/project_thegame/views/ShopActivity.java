2004619a5f3584670d58c0069f7277e0e8278d02
Barbera99 <josep.barbera.reverte.1999@gmail.com>
2022-05-24 14:58:31 +0100

    Afegir funcions per als botons

--- app/src/main/java/com/example/project_thegame/views/ShopActivity.java
+++ app/src/main/java/com/example/project_thegame/views/ShopActivity.java
@@ -2,18 +2,46 @@ package com.example.project_thegame.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.example.project_thegame.R;
+import com.example.project_thegame.models.Deck;
 
 public class ShopActivity extends AppCompatActivity {
+    private static final String TAG = "ShopActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
+        ImageButton home;
+        ImageButton deck;
+
+        home = findViewById(R.id.home);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goTo(MainActivity.class);
+            }
+        });
+
+        deck = findViewById(R.id.deck);
+        deck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goTo(DeckActivity.class);
+            }
+        });
     }
 
-
+    public void goTo(Class _class){
+        Log.d(TAG, "IntGoto; ");
+        Intent intent = new Intent(this, _class);
+        startActivity(intent);
+    }
 
 }
